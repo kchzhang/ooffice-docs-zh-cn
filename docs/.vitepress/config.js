ee854c6bc5c67d9dcf22f4287cd6fb87dcfb0e28
zhangkc <zhangkc@fadada.com>
2024-01-25 10:45:25 +0800

    init

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -505,17 +505,31 @@ export default {
         text: "免费产品",
         collapsible: true,
         items: [
-          { text: "文档服务器-7.1.1", link: "/paid-custom-free/7.1.1.23" },
-          { text: "文档服务器-7.5.1", link: "/paid-custom-free/7.5.1.23" },
+          { text: "文档服务-7.1.1", link: "/paid-custom-free/7.1.1.23" },
+          { text: "文档服务-7.5.1", link: "/paid-custom-free/7.5.1.23" },
         ],
       },
       {
         text: "付费产品",
         collapsible: true,
         items: [
-          { text: "文档服务器-7.1.1", link: "/paid-custom-image/" },
-          { text: "文档服务器-7.5.1", link: "/paid-custom-image/7.5.1.23/" },
-          { text: "文档生成器-7.5.1", link: "/paid-custom-builder/7.5.1.23/" },
+          {
+            text: "文档服务",
+            collapsible: false,
+            items: [
+              { text: "documentserver-7.1.1", link: "/paid-custom-image/7.1.1.23/" },
+              { text: "documentserver-7.5.1", link: "/paid-custom-image/7.5.1.23/" },
+            ]
+          },
+          // { text: "文档服务器-7.1.1", link: "/paid-custom-image/" },
+          // { text: "文档服务器-7.5.1", link: "/paid-custom-image/7.5.1.23/" },
+          {
+            text: "文档生成器",
+            collapsible: false,
+            items: [
+              { text: "documentbuilder-7.5.1", link: "/paid-custom-builder/7.5.1.23/" },
+            ]
+          },
           { text: "连接器", link: "/paid-custom-image/connector/" },
           { text: "定制服务", link: "/paid-custom/" },
           { text: "疑难解答", link: "/paid-chat/" },
---
+++ docs/.vitepress/dist/assets/chunks/cell.6b4448b3.js
@@ -0,0 +1,1 @@
+const o="/ooffice-docs-zh-cn/pay/cell.png";export{o as _};
